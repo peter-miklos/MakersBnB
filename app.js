2e533577b482c11b46a98cf229a3204cd1113fa3
johnnydee8 <j.dworzynski@me.com>
2016-09-19 17:40:52 +0100

    initial project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello, Makers B&B welcomes you!!');
+});
+
+app.listen(3000, function () {
+  console.log('Makers B&B app listening on port 3000!');
+});
+
+
+// https://expressjs.com/en/guide/routing.html - instruction how to create seperate files with routing
