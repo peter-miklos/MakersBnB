421533ead42b06eb52e118a6c4da8253a01fc834
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-09-20 12:32:39 +0100

    post method for listing added

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set("view engine", "ejs");
 
 app.get('/', function (req, res) {
@@ -10,7 +14,17 @@ app.get('/', function (req, res) {
 
 app.get("/listings/new", function (req, res) {
   res.render("listings/new", {});
-})
+});
+
+app.post("/listings", function (req, res) {
+  var name = req.body.name,
+      description = req.body.description,
+      price = req.body.price,
+      available_from = req.body.available_from,
+      available_to = req.body.available_to;
+  console.log(name);
+  res.redirect("/");
+});
 
 app.listen(3000, function () {
   console.log('Makers B&B app listening on port 3000!');
