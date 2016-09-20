d17c356724a19de9e9dc5fa7572a315ccc27e118
johnnydee8 <j.dworzynski@me.com>
2016-09-20 15:12:33 +0100

    db connection works now trying to extract this between separate files

--- models/listing.js
+++ models/listing.js
@@ -1,16 +1,13 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-var ListingsSchema = new Schema ({
+var listingSchema = mongoose.Schema({
   name: String,
   description: String,
   price: Number,
   availableFrom: Date,
   availableTo: Date,
-  // user: {
-  //   type: Schema.ObjectId,
-  //   ref: 'users'
-  // }
 });
 
-mongoose.model('listings', ListingsSchema);
+var Listing = mongoose.model('Listing', listingSchema);
+
+console.log('This file (listing.js) is loaded too!')
